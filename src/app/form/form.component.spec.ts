e5832a93263e3e605ea9faa9b6923702ea44eef8
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-09-22 17:32:13 +0900

    update form.component.spec.ts

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -37,4 +37,48 @@ describe('FormComponent', () => {
   it('should compile', () => {
     expect(component).toBeTruthy();
   });
+
+  it('firstName should be validation error', () => {
+    const firstName = component.addressForm.get('firstName');
+    firstName.setValue('');
+    expect(firstName.hasError('required')).toBeTruthy();
+  });
+
+  it('postalCode should be validation error of required', () => {
+    const postalCode = component.addressForm.get('postalCode');
+    postalCode.setValue('');
+    expect(postalCode.hasError('required')).toBeTruthy();
+  });
+
+  it('postalCode should be validation error of required', () => {
+    const postalCode = component.addressForm.get('postalCode');
+    postalCode.setValue('');
+    expect(postalCode.hasError('required')).toBeTruthy();
+  });
+
+  it('postalCode should be validation error of minlength', () => {
+    const postalCode = component.addressForm.get('postalCode');
+    postalCode.setValue('123');
+    expect(postalCode.hasError('minlength')).toBeTruthy();
+  });
+
+  it('postalCode should be validation error of maxlength', () => {
+    const postalCode = component.addressForm.get('postalCode');
+    postalCode.setValue('123456');
+    expect(postalCode.hasError('maxlength')).toBeTruthy();
+  });
+
+  it('number of textarea element should be two', () => {
+    component.hasUnitNumber = true;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const elementList = compiled.querySelectorAll('textarea');
+    expect(elementList.length).toEqual(2);
+  });
+
+  it('state should be validation error of required', () => {
+    const state = component.addressForm.get('state');
+    state.setValue('');
+    expect(state.hasError('required')).toBeTruthy();
+  });
 });
